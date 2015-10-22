6273af8b852fa6ec82705112b1a1107b1fd4d406
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-10-22 14:00:58 -0400

    Puts enter after Detector tag for grasp

--- gpsans_to_grasp/convert_to_grasp.py
+++ gpsans_to_grasp/convert_to_grasp.py
@@ -31,12 +31,14 @@ def parse_args():
     parser.add_argument('-i', '--infile', help='Mantid Corrected Nexus file to read the data', required=True)
     parser.add_argument('-t', '--templatefile', help='Instrument RAW data template file.', required=True)
     parser.add_argument('-o', '--outfile', help='Output file (data from the nexus file + metadata from the raw file)', required=True)
+    parser.add_argument('-p', '--plot', help='Ploting options',  choices=['linear','log'], required=False)
     args = vars(parser.parse_args())
     return args
 
 def plot2d(data):
     logger.debug("Plotting 2D linear...")
     import matplotlib.pyplot as plt
+    data = np.rot90(data,3)
     plt.imshow(data,
         aspect=0.5)
     plt.show()
@@ -45,6 +47,7 @@ def plot2d_log(data):
     logger.debug("Plotting 2D log...")
     import matplotlib.pyplot as plt
     from matplotlib.colors import LogNorm
+    data = np.rot90(data,3)
     plt.imshow(data,
         #cmap=plt.get_cmap("rainbow"),
         norm=LogNorm(vmin=np.min(data), vmax=np.max(data)),
@@ -75,6 +78,25 @@ def read_data(filename):
     logger.debug("Data shape = %s." % str(data.shape))
     return data
 
+def prettify(elem):
+    """
+    Return a pretty-printed XML string for the Element.
+    I also put's a new line after tag: <Detector type=(.*)>
+    As GRASP does not read the data without this \n
+    """
+    from xml.etree import ElementTree
+    from xml.dom import minidom
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    pretty_str = reparsed.toprettyxml(indent="  ", newl="", encoding="utf-8")
+    # <Detector type="INT32[192,256]">
+    import re
+    pretty_str = re.sub(r'<Detector type=(.*)>',
+       '<Detector type=\\1>\n',
+        pretty_str)
+    return pretty_str
+
+
 def parse_xml(filename_in, filename_out, key_values_substitution):
     """
     Parses the XML file and will substitute
@@ -91,8 +113,11 @@ def parse_xml(filename_in, filename_out, key_values_substitution):
             tag.text = v
         else:
             logger.error("%s does not exist in the file %s." %(k,filename_in))
+    pretty_xml = prettify(root)
+    #tree.write(filename_out)
     logger.info("Writting data to %s...."%filename_out)
-    tree.write(filename_out)
+    with open(filename_out, "w") as text_file:
+        text_file.write(pretty_xml)
 
 def numpy_array_to_string(data):
     import io
@@ -106,7 +131,10 @@ def main(argv):
 
     # # Set negative to 0
     data_to_plot = data.clip(min=0.01)
-    plot2d_log(data_to_plot)
+    if args['plot'] == 'log':
+        plot2d_log(data_to_plot)
+    elif args['plot'] == 'linear':
+        plot2d(data_to_plot)
 
     parse_xml(args['templatefile'], args['outfile'],
         {'Data/Detector': numpy_array_to_string(data) })
